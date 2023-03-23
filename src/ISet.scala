e29726850570b4688b35b7a4b7b0342c4693f359
CapitaineToinon <antoinesutter@gmail.com>
2023-03-23 19:36:34 +0100

    scalafmt

--- src/ISet.scala
+++ src/ISet.scala
@@ -1,24 +1,24 @@
 package ch.hepia.tp
 
 //Ensemble d'éléments de type T
-case class ISet[T]( contains: T=>Boolean ) {
+case class ISet[T](contains: T => Boolean) {
 
   /* Ajoute un seul élément à l'ensemble.
    * A IMPLEMENTER
    */
-  def add( elt: T ): ISet[T] = this
+  def add(elt: T): ISet[T] = this
   /* Union d'ensembles.
    * A IMPLEMENTER
    */
-  def union( that: ISet[T] ): ISet[T] = this
+  def union(that: ISet[T]): ISet[T] = this
   /* Intersection d'ensembles.
    * A IMPLEMENTER
    */
-  def intersection( that: ISet[T] ): ISet[T] = this
+  def intersection(that: ISet[T]): ISet[T] = this
   /* Différence d'ensembles
    * A IMPLEMENTER
    */
-  def difference( that: ISet[T] ): ISet[T] = this
+  def difference(that: ISet[T]): ISet[T] = this
 
 }
 
@@ -27,11 +27,11 @@ object ISet {
   /*
    * Crée un ensemble vide.
    */
-  def empty[T] = ISet[T]( _ => false )
+  def empty[T] = ISet[T](_ => false)
   /*
    * Crée un ensemble avec un seul élément
    * A IMPLEMENTER
    */
-  def single[T]( t: T ) = empty[T]
+  def single[T](t: T) = empty[T]
 
 }
