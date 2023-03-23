6367cade26e6d20477e95b5a55992023c354bda6
CapitaineToinon <antoinesutter@gmail.com>
2023-03-23 20:00:05 +0100

    Done with ISet

--- src/ISet.scala
+++ src/ISet.scala
@@ -2,28 +2,32 @@ package ch.hepia.tp
 
 //Ensemble d'éléments de type T
 case class ISet[T](contains: T => Boolean) {
-
   /* Ajoute un seul élément à l'ensemble.
    * A IMPLEMENTER
    */
-  def add(elt: T): ISet[T] = this
+  def add(elt: T): ISet[T] = this.union(ISet.single(elt))
+
   /* Union d'ensembles.
    * A IMPLEMENTER
    */
-  def union(that: ISet[T]): ISet[T] = this
+  def union(that: ISet[T]): ISet[T] =
+    ISet[T](i => this.contains(i) || that.contains(i))
+
   /* Intersection d'ensembles.
    * A IMPLEMENTER
    */
-  def intersection(that: ISet[T]): ISet[T] = this
+  def intersection(that: ISet[T]): ISet[T] =
+    ISet[T](i => this.contains(i) && that.contains(i))
+
   /* Différence d'ensembles
    * A IMPLEMENTER
    */
-  def difference(that: ISet[T]): ISet[T] = this
+  def difference(that: ISet[T]): ISet[T] =
+    ISet[T](i => this.contains(i) && !that.contains(i))
 
 }
 
 object ISet {
-
   /*
    * Crée un ensemble vide.
    */
@@ -32,6 +36,5 @@ object ISet {
    * Crée un ensemble avec un seul élément
    * A IMPLEMENTER
    */
-  def single[T](t: T) = empty[T]
-
+  def single[T](t: T) = ISet[T](i => i == t)
 }
